b0a904ecb4c484024c6604743d2a707a632b2825
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-01-26 20:18:36 -0800

    disable Spawn Notification playing shiny sound when a Player sends out their Shiny pokemon. and also turn off some other logging shh it's not important

--- overrides/kubejs/server_scripts/legendary_encounters/setup.js
+++ overrides/kubejs/server_scripts/legendary_encounters/setup.js
@@ -7,7 +7,7 @@ ServerEvents.loaded(event => {
     let seed = event.getServer().worldData.worldGenOptions().seed()
     if(seed < 0)
         seed *= -1.0
-    console.log(seed)
+    //console.log(seed)
     global.findRegiPuzzles(seed)
-    Object.keys(global.regiPuzzleIndex).forEach(regi => console.log(`${regi} = ${global.regiPuzzleIndex[regi]}`))
+    //Object.keys(global.regiPuzzleIndex).forEach(regi => console.log(`${regi} = ${global.regiPuzzleIndex[regi]}`))
 })
